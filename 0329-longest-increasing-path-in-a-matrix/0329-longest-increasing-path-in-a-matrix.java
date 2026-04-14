5d2073e5d5218c5051f14fd8285b52d284e1e269
KSN <arunksn007@gmail.com>
2026-04-14 16:32:35 +0530

    Time: 7 ms (97.98%), Space: 47.3 MB (34.66%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -1,40 +1,51 @@
 class Solution {
-    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-    int m, n;
-
     public int longestIncreasingPath(int[][] matrix) {
-        m = matrix.length;
-        n = matrix[0].length;
-
-        int[][] dp = new int[m][n];
-
-        int max = 0;
-
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                max = Math.max(max, dfs(matrix, i, j, dp));
+        int dp[][] = new int [matrix.length][matrix[0].length];
+        int ans=0;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){    
+                int n= helper(i,j,matrix,dp);
+                ans= Math.max(ans,n);
             }
         }
-
-        return max;
+        return ans;
     }
 
-    private int dfs(int[][] matrix, int i, int j, int[][] dp) {
-        if(dp[i][j] != 0) return dp[i][j];
-
-        int maxLen = 1;
-
-        for(int[] d : dirs) {
-            int ni = i + d[0];
-            int nj = j + d[1];
-
-            if(ni >= 0 && nj >= 0 && ni < m && nj < n 
-               && matrix[ni][nj] > matrix[i][j]) {
-
-                maxLen = Math.max(maxLen, 1 + dfs(matrix, ni, nj, dp));
+    public int helper(int i, int j, int grid[][], int dp[][]){
+        if(dp[i][j]!=0){
+            return dp[i][j];
+        }
+        int ans=1;
+        // left
+        int nr = i;
+        int nc = j-1;
+        if(nc>=0){
+            if(grid[nr][nc]>grid[i][j]){
+                ans =Math.max(ans, 1+helper(nr,nc,grid,dp));
             }
         }
 
-        return dp[i][j] = maxLen;
+        // right
+        nr = i;
+        nc = j+1;
+        if(nc<grid[0].length && grid[nr][nc]>grid[i][j]){
+            ans =Math.max(ans, 1+helper(nr,nc,grid,dp));
+        }
+
+        // up
+        nr = i-1;
+        nc =j;
+        if(nr>=0 && grid[nr][nc]>grid[i][j]){
+            ans =Math.max(ans, 1+helper(nr,nc,grid,dp));
+        }
+
+        // down
+        nr = i+1;
+        nc =j;
+        if(nr<grid.length && grid[nr][nc]>grid[i][j]){
+            ans =Math.max(ans, 1+helper(nr,nc,grid,dp));
+        }
+
+        return dp[i][j]=ans;
     }
 }
