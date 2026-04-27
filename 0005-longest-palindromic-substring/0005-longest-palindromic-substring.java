adb97bed859a9abbf4cb385318b91e800d7fda4f
KSN <arunksn007@gmail.com>
2026-04-27 21:13:52 +0530

    Time: 18 ms (46.89%), Space: 46.9 MB (28.31%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,28 +1,31 @@
-class Solution {
+public class Solution {
     public String longestPalindrome(String s) {
-        int n = s.length();
-        String res = "";
-
-        for(int i = 0;i<n;i++){
-            
-            int st = i,end = i;
-            while(st>=0&&end<n&&s.charAt(st)==s.charAt(end)){
-                st--;
-                end++;
-            }
-            String temp = s.substring(st+1,end);
-            if(temp.length()>res.length())res=temp;
-
-            st = i;
-            end = i+1;
-            while(st>=0&&end<n&&s.charAt(st)==s.charAt(end)){
-                st--;
-                end++;
-            }
-            temp = s.substring(st+1,end);
-            if(temp.length()>res.length())res=temp;
+        if (s.length() <= 1) {
+            return s;
         }
 
-        return res;
+        String maxStr = s.substring(0, 1);
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            String odd = expandFromCenter(s, i, i);
+            String even = expandFromCenter(s, i, i + 1);
+
+            if (odd.length() > maxStr.length()) {
+                maxStr = odd;
+            }
+            if (even.length() > maxStr.length()) {
+                maxStr = even;
+            }
+        }
+
+        return maxStr;
+    }
+
+    private String expandFromCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return s.substring(left + 1, right);
     }
 }
